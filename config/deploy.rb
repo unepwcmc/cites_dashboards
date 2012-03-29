c2a9ce8a8765d0b0e4c41332ced16b89a6de640c
Décio Ferreira <decio.ferreira@decioferreira.com>
2012-03-29 15:15:16 +0100

    Capistrano installation

---
+++ config/deploy.rb
@@ -0,0 +1,74 @@
+set :application, "set your application name here"
+set :repository,  "set your repository location here"
+
+set :repository, "git@github.com:unepwcmc/cites_dashboards"
+set :scm, :git
+set :scm_username, "unepwcmc-read"
+
+# Primary domain name of your application. Used in the Apache configs
+set :domain, "unepwcmc-001.vm.brightbox.net"
+
+## List of servers
+server "unepwcmc-001.vm.brightbox.net", :app, :web, :db, :primary => true
+
+set :branch, "master"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+## Local Shared Area
+# These are the list of files and directories that you want
+# to share between the releases of your application on a particular
+# server. It uses the same shared area as the log files.
+#
+# NOTE: local areas trump global areas, allowing you to have some
+# servers using local assets if required.
+#
+# So if you have an 'upload' directory in public, add 'public/upload'
+# to the :local_shared_dirs array.
+# If you want to share the database.yml add 'config/database.yml'
+# to the :local_shared_files array.
+#
+# The shared area is prepared with 'deploy:setup' and all the shared
+# items are symlinked in when the code is updated.
+# set :local_shared_dirs, %w(public/upload)
+# set :local_shared_files, %w(config/database.yml)
+set :global_shared_dirs, %w(public/system)
+set :global_shared_files, %w(config/database.yml)
+
+# Forces a Pty so that svn+ssh repository access will work. You
+# don't need this if you are using a different SCM system. Note that
+# ptys stop shell startup scripts from running.
+default_run_options[:pty] = true
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+task :setup_production_database_configuration do
+  the_host = Capistrano::CLI.ui.ask("Database IP address: ")
+  database_name = Capistrano::CLI.ui.ask("Database name: ")
+  database_user = Capistrano::CLI.ui.ask("Database username: ")
+  pg_password = Capistrano::CLI.password_prompt("Database user password: ")
+
+  require 'yaml'
+
+  spec = {
+    "#{rails_env}" => {
+      "adapter" => "postgresql",
+      "database" => database_name,
+      "username" => database_user,
+      "host" => the_host,
+      "password" => pg_password
+    }
+  }
+
+  run "mkdir -p #{shared_path}/config"
+  put(spec.to_yaml, "#{shared_path}/config/database.yml")
+end
+after "deploy:setup", :setup_production_database_configuration
--- config/environment.rb
+++ config/environment.rb
@@ -13,6 +13,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.gem "pg"
   config.gem "googlecharts", :lib => "gchart"
+  config.gem "capistrano"
   #config.gem "composite_primary_keys"
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
