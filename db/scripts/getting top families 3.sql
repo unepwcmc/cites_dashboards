8b7b263955e6a8e0406c92b7390b2d04c066928a
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-10-01 11:31:57 +0100

    Tidy up the file to respect the 80columns thingy

--- db/scripts/getting top families 3.sql
+++ db/scripts/getting top families 3.sql
@@ -24,13 +24,13 @@ FROM
 				    WHEN shipment_year BETWEEN 1976 AND 1980  THEN 1980
 				    WHEN shipment_year BETWEEN 1975 AND 1975  THEN 1975
 				    ELSE 1900
-          END as shipment_year, 
-          'Global' as import_country_code, 'Global' as export_country_code, 
-          'All' as source_code, 
-          --unit_code_1, purpose_code, 		
-          approved.taxon_family, approved.taxon_group, term_code_1,unit_code_1, 
+          END as shipment_year,
+          'Global' as import_country_code, 'Global' as export_country_code,
+          'All' as source_code,
+          --unit_code_1, purpose_code,
+          approved.taxon_family, approved.taxon_group, term_code_1,unit_code_1,
           SUM(quantity_1) as quantity_1
-        FROM national_detail 
+        FROM national_detail
           inner join cites_taxon_codes as original
             on national_detail.cites_taxon_code = original.cites_taxon_code
           inner join cites_taxon_codes as approved
@@ -52,7 +52,7 @@ INSERT INTO top_families(shipment_year, taxon_group, reporter_type,term_code,uni
 SELECT *
 FROM
   (SELECT shipment_year, taxon_group, reporter_type, term_code_1,unit_code_1, source_code,
-    import_country_code,export_country_code, taxon_family, quantity_1, 
+    import_country_code,export_country_code, taxon_family, quantity_1,
           rank() OVER (PARTITION BY shipment_year, reporter_type,taxon_group,term_code_1,
             unit_code_1, source_code ORDER BY quantity_1 DESC, taxon_family) AS pos
                FROM (SELECT reporter_type, CASE
@@ -64,14 +64,14 @@ FROM
 				    WHEN shipment_year BETWEEN 1981 AND 1985  THEN 1985
 				    WHEN shipment_year BETWEEN 1976 AND 1980  THEN 1980
 				    WHEN shipment_year BETWEEN 1975 AND 1975  THEN 1975
-				    ELSE 1900 
-		END as shipment_year, 
-		'Global' as import_country_code, 'Global' as export_country_code, 
-		source_code, 
-		--unit_code_1, purpose_code, 		
+				    ELSE 1900
+		END as shipment_year,
+		'Global' as import_country_code, 'Global' as export_country_code,
+		source_code,
+		--unit_code_1, purpose_code,
 		approved.taxon_family,approved.taxon_group, term_code_1,unit_code_1,
 		SUM(quantity_1) as quantity_1
-		FROM national_detail 
+		FROM national_detail
 			inner join cites_taxon_codes as original on
        national_detail.cites_taxon_code = original.cites_taxon_code
 			inner join cites_taxon_codes as approved on
@@ -91,7 +91,7 @@ INSERT INTO top_families(shipment_year, taxon_group, reporter_type,term_code,uni
 SELECT *
 FROM
   (SELECT shipment_year, taxon_group, reporter_type, term_code_1,unit_code_1, source_code,
-    import_country_code,export_country_code, taxon_family, quantity_1, 
+    import_country_code,export_country_code, taxon_family, quantity_1,
           rank() OVER (PARTITION BY shipment_year, reporter_type,taxon_group,term_code_1,
             unit_code_1,import_country_code ORDER BY quantity_1 DESC, taxon_family) AS pos
      FROM (SELECT reporter_type, CASE
@@ -102,14 +102,14 @@ FROM
 				    WHEN shipment_year BETWEEN 1984 AND 1988  THEN 1988
 				    WHEN shipment_year BETWEEN 1979 AND 1983  THEN 1983
 				    WHEN shipment_year BETWEEN 1974 AND 1978  THEN 1978
-				    ELSE 1900 
-		END as shipment_year, 
-		import_country_code, 'N/A' as export_country_code, 
-		'All' as source_code, 
-		--unit_code_1, purpose_code, 		
+				    ELSE 1900
+		END as shipment_year,
+		import_country_code, 'N/A' as export_country_code,
+		'All' as source_code,
+		--unit_code_1, purpose_code,
 		approved.taxon_family, approved.taxon_group, term_code_1,unit_code_1,
 		SUM(quantity_1) as quantity_1
-		FROM national_detail 
+		FROM national_detail
 			inner join cites_taxon_codes as original on
         national_detail.cites_taxon_code = original.cites_taxon_code
 			inner join cites_taxon_codes as approved on
@@ -139,14 +139,14 @@ FROM
 				    WHEN shipment_year BETWEEN 1984 AND 1988  THEN 1988
 				    WHEN shipment_year BETWEEN 1979 AND 1983  THEN 1983
 				    WHEN shipment_year BETWEEN 1974 AND 1978  THEN 1978
-				    ELSE 1900 
-		END as shipment_year, 
-		'N/A' as import_country_code,export_country_code, 
-		'All' as source_code, 
-		--unit_code_1, purpose_code, 		
-		approved.taxon_family, approved.taxon_group, term_code_1,unit_code_1, 
+				    ELSE 1900
+		END as shipment_year,
+		'N/A' as import_country_code,export_country_code,
+		'All' as source_code,
+		--unit_code_1, purpose_code,
+		approved.taxon_family, approved.taxon_group, term_code_1,unit_code_1,
 		SUM(quantity_1) as quantity_1
-		FROM national_detail 
+		FROM national_detail
 			inner join cites_taxon_codes as original on
        national_detail.cites_taxon_code = original.cites_taxon_code
 			inner join cites_taxon_codes as approved on
@@ -176,14 +176,14 @@ FROM
 				    WHEN shipment_year BETWEEN 1984 AND 1988  THEN 1988
 				    WHEN shipment_year BETWEEN 1979 AND 1983  THEN 1983
 				    WHEN shipment_year BETWEEN 1974 AND 1978  THEN 1978
-				    ELSE 1900 
-		END as shipment_year, 
-		import_country_code, 'N/A' as export_country_code, 
-		source_code, 
-		--unit_code_1, purpose_code, 		
-		approved.taxon_family, approved.taxon_group, term_code_1,unit_code_1, 
+				    ELSE 1900
+		END as shipment_year,
+		import_country_code, 'N/A' as export_country_code,
+		source_code,
+		--unit_code_1, purpose_code,
+		approved.taxon_family, approved.taxon_group, term_code_1,unit_code_1,
 		SUM(quantity_1) as quantity_1
-		FROM national_detail 
+		FROM national_detail
 			inner join cites_taxon_codes as original on
         national_detail.cites_taxon_code = original.cites_taxon_code
 			inner join cites_taxon_codes as approved on
@@ -202,7 +202,7 @@ INSERT INTO top_families(shipment_year, taxon_group, reporter_type,term_code,uni
 SELECT *
 FROM
   (SELECT shipment_year, taxon_group, reporter_type, term_code_1,unit_code_1, source_code,
-    import_country_code,export_country_code, taxon_family, quantity_1, 
+    import_country_code,export_country_code, taxon_family, quantity_1,
     rank() OVER (PARTITION BY shipment_year, reporter_type,taxon_group,term_code_1,unit_code_1,
     export_country_code,source_code ORDER BY quantity_1 DESC, taxon_family) AS pos
      FROM (SELECT reporter_type, CASE
@@ -213,14 +213,14 @@ FROM
 				    WHEN shipment_year BETWEEN 1984 AND 1988  THEN 1988
 				    WHEN shipment_year BETWEEN 1979 AND 1983  THEN 1983
 				    WHEN shipment_year BETWEEN 1974 AND 1978  THEN 1978
-				    ELSE 1900 
-		END as shipment_year, 
-		'N/A' as import_country_code, export_country_code, 
-		source_code, 
-		--unit_code_1, purpose_code, 		
-		approved.taxon_family, approved.taxon_group, term_code_1,unit_code_1, 
+				    ELSE 1900
+		END as shipment_year,
+		'N/A' as import_country_code, export_country_code,
+		source_code,
+		--unit_code_1, purpose_code,
+		approved.taxon_family, approved.taxon_group, term_code_1,unit_code_1,
 		SUM(quantity_1) as quantity_1
-		FROM national_detail 
+		FROM national_detail
 			inner join cites_taxon_codes as original on
         national_detail.cites_taxon_code = original.cites_taxon_code
 			inner join cites_taxon_codes as approved on
@@ -234,13 +234,13 @@ FROM
 WHERE pos < 11;
 
 --The family data
-delete from families_trade_summaries; 
+delete from families_trade_summaries;
 insert into families_trade_summaries (shipment_year,appendix,origin_country_code,reporter_type,
   import_country_code,export_country_code,term_code,unit_code,quantity,source_code,purpose_code,
   taxon_group,taxon_family )
 select n.shipment_year,appendix,origin_country_code,n.reporter_type,n.import_country_code,
   n.export_country_code,term_code_1,unit_code_1,sum(quantity_1),n.source_code,n.purpose_code,
-  cites_taxon_codes.taxon_group, cites_taxon_codes.taxon_family 
+  cites_taxon_codes.taxon_group, cites_taxon_codes.taxon_family
 from national_detail n
 	--inner join top_families on national_detail.cites_taxon_code = top_families.cites_taxon_code
 	inner join cites_taxon_codes on n.cites_taxon_code = cites_taxon_codes.cites_taxon_code
@@ -254,15 +254,17 @@ group by n.shipment_year,appendix,origin_country_code,n.reporter_type,n.import_c
   n.export_country_code,term_code_1,unit_code_1,n.source_code,n.purpose_code, cites_taxon_codes.taxon_group,
   cites_taxon_codes.taxon_family;
 
-update top_families n set group_term_id = t.id from group_terms t 
-	where (n.taxon_group = t.taxon_group and n.term_code = t.term_code and n.unit_code = t.unit_code)
-	  or  (n.taxon_group = t.taxon_group and n.term_code = t.term_code and n.unit_code is null and t.unit_code is null);
+update top_families n set group_term_id = t.id from group_terms t
+	where (n.taxon_group = t.taxon_group and n.term_code = t.term_code
+    and n.unit_code = t.unit_code)
+	  or  (n.taxon_group = t.taxon_group and n.term_code = t.term_code
+      and n.unit_code is null and t.unit_code is null);
 
-update families_trade_summaries n set group_term_id = t.id from group_terms t 
-	where (n.taxon_group = t.taxon_group and n.term_code = t.term_code and n.unit_code = t.unit_code)
-	  or  (n.taxon_group = t.taxon_group and n.term_code = t.term_code and n.unit_code is null and t.unit_code is null);
+update families_trade_summaries n set group_term_id = t.id from group_terms t
+	where (n.taxon_group = t.taxon_group and n.term_code = t.term_code
+    and n.unit_code = t.unit_code)
+	  or  (n.taxon_group = t.taxon_group and n.term_code = t.term_code
+      and n.unit_code is null and t.unit_code is null);
 
 --select * from top_families
 --select * from families_trade_summaries
-
-
