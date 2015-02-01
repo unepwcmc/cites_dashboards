c80d064b3ddf6ecc6e41ea4dde2b5c7bb9bb20ad
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-02-01 11:30:48 +0000

    removed obsolete scripts

--- db/scripts/term_codes.sql
+++
@@ -1,7 +0,0 @@
-update national_trade_summaries n set group_term_id = t.id from group_terms t 
-  where (n.taxon_group = t.taxon_group and n.term_code = t.term_code and n.unit_code = t.unit_code)
-    or  (n.taxon_group = t.taxon_group and n.term_code = t.term_code and n.unit_code is null and t.unit_code is null);
-
-update global_trade_summaries n set group_term_id = t.id from group_terms t 
-  where (n.taxon_group = t.taxon_group and n.term_code = t.term_code and n.unit_code = t.unit_code)
-    or  (n.taxon_group = t.taxon_group and n.term_code = t.term_code and n.unit_code is null and t.unit_code is null);
